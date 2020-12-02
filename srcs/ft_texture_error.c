c486c012156e26af6d89b030fde87a48d68d5be0
Youssef Noam <ynoam@e1r8p6.1337.ma>
2020-12-02 12:50:37 +0100

    don

--- srcs/ft_texture_error.c
+++ srcs/ft_texture_error.c
@@ -6,7 +6,7 @@
 /*   By: ynoam <ynoam@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/17 19:08:36 by ynoam             #+#    #+#             */
-/*   Updated: 2020/10/18 11:44:12 by ynoam            ###   ########.fr       */
+/*   Updated: 2020/12/02 12:49:53 by ynoam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 int	ft_texture_error(char *str)
 {
-	ft_putstr_fd("wolf: error: ", 2);
+	ft_putstr_fd("Error\n", 2);
+	ft_putstr_fd("cub3D: error: ", 2);
 	ft_putstr_fd(str, 2);
 	exit(EXIT_FAILURE);
 	return (1);
